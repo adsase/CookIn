543eb8ad038085a8b9c4473d2eb1e37e250f3403
adsase <adriansalomsempere@hotmail.com>
2015-02-02 02:52:06 +0100

    Initial commit

---
+++ app/src/main/java/dam2/sixapp/cookin/swipeTabs/userMain/Wololo.java
@@ -0,0 +1,15 @@
+package dam2.sixapp.cookin.swipeTabs.userMain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import dam2.sixapp.cookin.R;
+
+public class Wololo extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layout);
+    }
+}
