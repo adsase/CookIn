6204d2d550f01d028a728dc9dc050d321baab8b5
adsase <adriansalomsempere@hotmail.com>
2015-02-04 16:34:03 +0100

    Va de bo!

---
+++ app/src/main/java/dam2/sixapp/cookin/collaborate/Collaborate.java
@@ -0,0 +1,46 @@
+package dam2.sixapp.cookin.collaborate;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import dam2.sixapp.cookin.R;
+import dam2.sixapp.cookin.drawer.NavigationDrawerFragment;
+//
+
+
+public class Collaborate extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colabora_layout);
+
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
